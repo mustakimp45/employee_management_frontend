871ae764eddf077948ed01b25b7a17f5cae90684
ANOOP JAISWAL <anoopjaiwal903@gmail.com>
2022-11-08 09:42:03 +0530

    commit here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// import SearchBar from "./components/SearchBar";
+// import BookData from "./Data.json";
 
-import SearchBar from "./components/SearchBar";
-import BookData from "./Data.json";
-
-import AddUser from './AddUser';
-import Home from './Home';
+import AddUser from "./AddUser";
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <SearchBar placeholder="Enter a Book Name..." data={BookData} />
+      {/* <SearchBar placeholder="Enter a Book Name..." data={BookData} /> */}
       <Routes>
         <Route path="/" />
         <Route path="/AddUser" element={<AddUser />} />
-        <Route path="/contact" element={<Home />}/>
+        <Route path="/contact" element={<Home />} />
         <Route path="/about" />
       </Routes>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import SearchBar from "./SearchBar";
 import {
   NavbarContainer,
   LeftContainer,
@@ -33,9 +34,7 @@ function Navbar() {
             </OpenLinksButton>
           </NavbarLinkContainer>
         </LeftContainer>
-        <RightContainer>
-          {/* <Logo src={LogoImg}></Logo> */}
-        </RightContainer>
+        <RightContainer>{/* <Logo src={LogoImg}></Logo> */}</RightContainer>
       </NavbarInnerContainer>
       {extendNavbar && (
         <NavbarExtendedContainer>
@@ -43,6 +42,7 @@ function Navbar() {
           <NavbarLinkExtended to="/AddUser"> Add User</NavbarLinkExtended>
           <NavbarLinkExtended to="/contact"> Contact Us</NavbarLinkExtended>
           <NavbarLinkExtended to="/about"> About Us</NavbarLinkExtended>
+          <SearchBar placeholder="Enter a Book Name..." data={BookData} />
         </NavbarExtendedContainer>
       )}
     </NavbarContainer>
